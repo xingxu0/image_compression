182bfe71fe61428f8da3646a67909f747a37e80d
Xing <xing@xing-Studio-XPS-7100.(none)>
2014-10-09 17:51:36 -0700

    backup entropy 2 dimensions

---
+++ entropy_libjpeg/jpeg-9/test_10_images.py
@@ -0,0 +1,5 @@
+import os
+
+for i in range(1, 11):
+	os.system("jpegtran -encode ../../../image_compression_github/image_compression/tbl_600_q75_1 ../../images/10_pic_600/" + str(i) + ".jpg temp.jpg")
+	os.system("jpegtran -decode ../../../image_compression_github/image_compression/tbl_600_q75_1 temp.jpg " + str(i) + "_out.jpg")
