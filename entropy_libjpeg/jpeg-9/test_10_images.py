b5736a71df4fc8aa4cf1ced2d9c7412802599bf4
Xing <xing@xing-Studio-XPS-7100.(none)>
2014-10-14 17:37:01 -0700

    a

--- entropy_libjpeg/jpeg-9/test_10_images.py
+++ entropy_libjpeg/jpeg-9/test_10_images.py
@@ -1,5 +1,5 @@
 import os
 
 for i in range(1, 11):
-	os.system("jpegtran -encode ../../../image_compression_github/image_compression/tbl_600_q75_1 ../../images/10_pic_600/" + str(i) + ".jpg temp.jpg")
-	os.system("jpegtran -decode ../../../image_compression_github/image_compression/tbl_600_q75_1 temp.jpg " + str(i) + "_out.jpg")
+	os.system("jpegtran -encode ../../tbl_600_q75_1 ../../../../image_compression/images/10_pic_600/" + str(i) + ".jpg temp.jpg")
+	os.system("jpegtran -decode ../../tbl_600_q75_1 temp.jpg " + str(i) + "_out.jpg")
