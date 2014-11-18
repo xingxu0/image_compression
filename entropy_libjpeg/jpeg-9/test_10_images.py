a9de1146e7fc064101fb3781fe1acfb1ec496dd8
Xing <vess527@gmail.com>
2014-11-18 13:20:46 -0800

    a

--- entropy_libjpeg/jpeg-9/test_10_images.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for i in range(1, 11):
-	os.system("jpegtran -encode ../../tbl_600_q75_1 ../../../../image_compression/images/10_pic_600/" + str(i) + ".jpg temp.jpg")
-	os.system("jpegtran -decode ../../tbl_600_q75_1 temp.jpg " + str(i) + "_out.jpg")
