b16ca07d4d2735ef1d5bc96aece6dab7c8964d05
Xing <vess527@gmail.com>
2014-12-31 16:22:06 -0800

    a

--- training_better_DC.py
+++ training_better_DC.py
@@ -114,7 +114,7 @@ def create_table(comp, dep1_s, dep2_s):
 			if last_dc_diff < 0:
 				last_dc_diff_bits = -last_dc_diff_bits
 			if dc_diff < 0:
-				dc_diff_bits = - dc_diff_bits
+				dc_diff_bits = -dc_diff_bits
 			oc_dc[last_dc_diff_bits+11][dc_diff_bits+11] += 1
 			#oc_dc[lib.get_previous_blocks_coef_for_DC(block, ii)][b[0]] += 1
 			r = 0
