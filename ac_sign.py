6f4b161d771c1ad1ee3a30a3f84caaa153e403d8
Xing <vess527@gmail.com>
2015-02-01 23:05:15 -0800

    a

--- ac_sign.py
+++ ac_sign.py
@@ -5,7 +5,7 @@ from operator import itemgetter
 
 def analyze(oc,i,p,pp):
 	global tab_folder
-	fname = table_folder + "/" + str(i)+"_"+str(d1)+"_"+str(d2)+".table"
+	fname = tab_folder + "/" + str(i)+"_"+str(d1)+"_"+str(d2)+".table"
 	f = open(fname, "w")
 	t_pos = 0
 	t_neg = 0
