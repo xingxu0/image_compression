2e518631bf238930bd78e2688df2c7979be45b79
Xing <vess527@gmail.com>
2015-02-23 15:44:33 -0800

    a

--- libjpeg-turbo-2_3_dc/jdhuff.h
+++ libjpeg-turbo-2_3_dc/jdhuff.h
@@ -15,7 +15,8 @@
 
 /* Derived data constructed for each Huffman table */
 
-#define HUFF_LOOKAHEAD  6       /* # of bits of lookahead */
+#define HUFF_LOOKAHEAD  8       /* # of bits of lookahead */
+#define HUFF_LOOKAHEAD_ENTROPY 6
 
 typedef struct {
   /* Basic tables: (element [0] of each array is unused) */
