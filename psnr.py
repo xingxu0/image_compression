55c6fe126d214f18aa352824487d3b299c5754c3
Xing <vess527@gmail.com>
2015-02-27 15:07:07 -0800

    a

--- psnr.py
+++ psnr.py
@@ -1,13 +1,16 @@
 import os, sys, glob
 
-q = sys.argv[1]
+fs = glob.glob("images/TESTIMAGES/RGB/RGB_R02_0600x0600/*.png")
 
-f = glob.glob("./*.png")
+thre = sys.argv[1]
 
-x = 0
-os.system("mkdir generate" + "_" + q)
-for i in f:
+q = "75"
+os.system("mkdir psnr_600_q75")
+for f in fs:
 	x += 1	
-	cmd = "convert -quality " + q  + " "  + i + " generate_" + q + "/" + str(x) +".jpg"
-	os.system(cmd)
-	print cmd
+	cmd = "convert -quality " + q  + " "  + f + " psnr_600_q75/" + str(x) +".jpg"
+	os.system("/opt/libjpeg-turbo/bin/jpegtran -outputcoef temp.block psnr_600_q75/%s.jpg temp.jpg"%(str(x)))
+	os.system("python lossy_zerooff.py temp.block temp_out.block " + thre)
+	os.system("/opt/libjpeg-turbo/bin/jpegtran -inputcoef temp_out.block temp.jpg psnr_600_q75/%s_zerooff.jpg"%(str(x)))
+	os.system("compare -metric PSNR " + f + " psnr_600_q75/%s.jpg temp_diff.png"%(str(x)))
+	os.system("compare -metric PSNR " + f + " psnr_600_q75/%s_zerooff.jpg temp_diff.png"%(str(x)))
