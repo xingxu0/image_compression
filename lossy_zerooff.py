5215742b7747cf334a1f6c34154902c30e72175b
Xing <vess527@gmail.com>
2015-03-05 13:21:33 -0800

    a

--- lossy_zerooff.py
+++ lossy_zerooff.py
@@ -10,6 +10,37 @@ import lib_new as lib
 #	print "usage: python runsize.py size(600, 1200), component_number(0,1,2) start_learn_image(1-100), end_learn_image(1-100), end_test_image(1-100), dep. 1(0:DC, 1:avg_pre_coef, 2:avg_pre_block_coef, 3:pre_block_coef, r:last_block_eob, 5:pre_blocks_sign), dep. 2(0:DC, 1:avg_pre_coef, 2:avg_pre_block_coef, 3:pre_block_coef, 4:last_block_eob, 5:pre_blocks_sign)"
 #	exit()
 
+quant = {}
+quant["0"] = [16,  11,  10,  16,  24,  40,  51,  61,
+  12,  12,  14,  19,  26,  58,  60,  55,
+  14,  13,  16,  24,  40,  57,  69,  56,
+  14,  17,  22,  29,  51,  87,  80,  62,
+  18,  22,  37,  56,  68, 109, 103,  77,
+  24,  35,  55,  64,  81, 104, 113,  92,
+  49,  64,  78,  87, 103, 121, 120, 101,
+  72,  92,  95,  98, 112, 100, 103,  99]
+
+quant["1"] = [17,  18,  24,  47,  99,  99,  99,  99,
+  18,  21,  26,  66,  99,  99,  99,  99,
+  24,  26,  56,  99,  99,  99,  99,  99,
+  47,  66,  99,  99,  99,  99,  99,  99,
+  99,  99,  99,  99,  99,  99,  99,  99,
+  99,  99,  99,  99,  99,  99,  99,  99,
+  99,  99,  99,  99,  99,  99,  99,  99,
+  99,  99,  99,  99,  99,  99,  99,  99]
+
+quant["2"] = quant["1"]
+
+jpeg_natural = [0,  1,  8, 16,  9,  2,  3, 10,
+ 17, 24, 32, 25, 18, 11,  4,  5,
+ 12, 19, 26, 33, 40, 48, 41, 34,
+ 27, 20, 13,  6,  7, 14, 21, 28,
+ 35, 42, 49, 56, 57, 50, 43, 36,
+ 29, 22, 15, 23, 30, 37, 44, 51,
+ 58, 59, 52, 45, 38, 31, 39, 46,
+ 53, 60, 61, 54, 47, 55, 62, 63,
+ 63, 63, 63, 63, 63, 63, 63, 63]
+
 def get_code(x, code):
 	s = x&15
 	r = x>>4
@@ -77,7 +108,8 @@ def zero_off(b, b_o, code, ci):
 			af = get_r_l(0, code)
 			af_code = 0
 			diff = get_code(x,code)+1-get_code(0, code)
-		if diff >= thre:
+		if diff >= thre*max(1, pow(quant[ci][jpeg_natural[p[i]]]/18, 2)):
+		#if diff >= thre:
 			if modified == 0:
 				#print " "
 				#print " "
