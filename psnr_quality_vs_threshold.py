be78d1a9e83f70079a54404809dc7acbab460612
vess527 <vess527@gmail.com>
2015-03-05 11:36:40 -0800

    a

--- psnr_quality_vs_threshold.py
+++ psnr_quality_vs_threshold.py
@@ -4,16 +4,18 @@ import os, sys, glob, commands
 import matplotlib.pyplot as plt
 from pylab import *
 
-def get_threshold_jpg(in_, out_, threshold):
+def get_threshold_jpg(out_, threshold, block_file, base_file):
 	global folder
-	c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -outputcoef temp.block %s temp.jpg"%(in_))
-	c = commands.getstatusoutput("python lossy_zerooff.py temp.block temp_out.block %s"%(threshold))
-	c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -inputcoef temp_out.block temp.jpg %s"%(out_))
+	c = commands.getstatusoutput("python lossy_zerooff.py %s tmp_out.block %s"%(block_file, threshold))
+	c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -inputcoef tmp_out.block %s %s"%(base_file, out_))
 
 fs = glob.glob("images/TESTIMAGES/RGB/RGB_R02_0600x0600/*.png")
+#fs = glob.glob("images/TESTIMAGES/RGB/RGB_OR_1200x1200/*.png")
 
-qs = [30,40,50,60,70,80,90]
-qs = [30, 50, 70]
+
+qs = range(60, 91)
+print qs
+#qs = [30, 50, 70]
 
 root_folder = "psnr_q_vs_t"
 c = commands.getstatusoutput("rm %s -rf"%(root_folder))
@@ -29,6 +31,8 @@ p_5_y = []
 p_10_x = []
 p_10_y = []
 for q in qs:
+	print ""
+	print q
 	folder = "%s/q_%s"%(root_folder, q)
 	c = commands.getstatusoutput("rm %s -rf"%(folder))
 	c = commands.getstatusoutput("mkdir %s"%(folder))
@@ -44,31 +48,33 @@ for q in qs:
 	size_3 = 0
 	size_5 = 0
 	size_10 = 0
+	#fs = fs[0:20]
 	for f in fs:
 		x += 1
-		print " "
-		print x,":","    ", 
+		#print " "
+		print x,f,":","    ", 
 		c = commands.getstatusoutput("convert -quality " + str(q)  + " "  + f + " " + folder + "/" + str(x) +".jpg")
-		get_threshold_jpg(folder+"/"+str(x)+".jpg", folder+"/"+str(x)+"_3.jpg", 3)
-		get_threshold_jpg(folder+"/"+str(x)+".jpg", folder+"/"+str(x)+"_5.jpg", 5)
-		get_threshold_jpg(folder+"/"+str(x)+".jpg", folder+"/"+str(x)+"_10.jpg", 10)
-		c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s.jpg temp_diff.png"%(folder, str(x)))
+		c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -outputcoef tmp.block %s %s"%(folder+"/"+str(x)+".jpg", folder+"/"+str(x)+"_std.jpg"))
+		get_threshold_jpg(folder+"/"+str(x)+"_3.jpg", 3, "tmp.block", folder+"/"+str(x)+"_std.jpg")
+		get_threshold_jpg(folder+"/"+str(x)+"_5.jpg", 5, "tmp.block", folder+"/"+str(x)+"_std.jpg")
+		get_threshold_jpg(folder+"/"+str(x)+"_10.jpg", 10, "tmp.block", folder+"/"+str(x)+"_std.jpg")
+		c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s.jpg tmp_diff.png"%(folder, str(x)))
 		print c[1], 
 		psnr_q += float(c[1])
-		c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s_3.jpg temp_diff.png"%(folder, str(x)))
-		print c[1]
+		c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s_3.jpg tmp_diff.png"%(folder, str(x)))
+		print c[1],
 		psnr_3 += float(c[1])
-		c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s_5.jpg temp_diff.png"%(folder, str(x)))
-		print c[1]
+		c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s_5.jpg tmp_diff.png"%(folder, str(x)))
+		print c[1],
 		psnr_5 += float(c[1])
-		c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s_10.jpg temp_diff.png"%(folder, str(x)))
+		c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s_10.jpg tmp_diff.png"%(folder, str(x)))
 		print c[1]
 		psnr_10 += float(c[1])
-		size_q += os.path.getsize("%s/%s.jpg"%(folder, str(x)))
+		size_q += os.path.getsize("%s/%s_std.jpg"%(folder, str(x)))
 		size_3 += os.path.getsize("%s/%s_3.jpg"%(folder, str(x)))
 		size_5 += os.path.getsize("%s/%s_5.jpg"%(folder, str(x)))
 		size_10 += os.path.getsize("%s/%s_10.jpg"%(folder, str(x)))
-		break
+	
 		
 	psnr_q /= len(fs)
 	psnr_3 /= len(fs)
@@ -87,16 +93,20 @@ for q in qs:
 	p_5_y.append(psnr_5)
 	p_10_y.append(psnr_10)
 
+print p_q_x, p_q_y
+print p_3_x, p_3_y
+print p_5_x, p_5_y
+print p_10_x, p_10_y
+exit(0)
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(p_q_x, p_q_y)
-ax.plot(p_q_3, p_q_3)
-ax.plot(p_q_5, p_q_5)
-ax.plot(p_q_10, p_q_10)
+ax.plot(p_q_x, p_q_y, '-x')
+ax.plot(p_3_x, p_3_y, '-o')
+ax.plot(p_5_x, p_5_y, '-s')
+ax.plot(p_10_x, p_10_y, '-d')
 ax.set_xlabel("file size (B)")
 ax.grid()
 ax.set_ylabel("PSNR (dB)")
 ax.legend(['original','th=3', 'th=5', 'th=10'], 4)
 savefig("psnr_quality_vs_threshold.png")
-
-
