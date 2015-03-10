78bdd1646a4a3df67c1d0fa881266e6c607f9761
vess527 <vess527@gmail.com>
2015-03-09 21:37:05 -0700

    a

---
+++ test_threshold.py
@@ -0,0 +1,66 @@
+import os, commands, re, sys
+
+folders = ['100', '200', '300', '400', '600', '800', '1000', '1200']
+folders = ['1200']
+
+x = []
+y_encoding = []
+y_decoding = []
+encoding = []
+decoding = []
+gain_ratio = []
+
+first_seen = False
+base_encode = 0
+base_decode = 0
+base_x = 0
+for f in folders:
+	total_optimized_size = 0
+	total_encoded_size = 0
+	total_encoding_time = 0
+	total_decoding_time = 0
+	for i in range(1, 101):#101):
+		c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -outputcoef " + str(i) + "_temp.block " +  "images/" + f + "_Q75/" + str(i) + ".jpg temp.jpg")
+		c = commands.getstatusoutput("python lossy_zerooff.py " + str(i) + "_temp.block py.block 0.003 75")
+		c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -encode tbl_" + "600" + "_23dc" + " images/" + f + "_Q75/" + str(i) + ".jpg temp.jpg")
+		print c
+		# sample output: Total saving: 134751 bits\nOriginal filesize: 106207, encoded filesize: 89350, saving: 0.158718\nTotal time elapsed : 65678 us'
+		m = re.match("Total saving: (.*) bits\nOriginal filesize: (.*), encoded filesize: (.*), saving: (.*)\nTotal time elapsed : (.*) us", c[1])
+		saving_bits = int(m.group(1))
+		jpg_opt_size = int(m.group(2))
+		out_size = int(m.group(3))
+		saving_percent = float(m.group(4))
+		encoding_time = int(m.group(5))
+		total_optimized_size += jpg_opt_size
+		total_encoded_size += out_size
+		total_encoding_time += encoding_time
+		#continue
+		c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -decode tbl_" + "600" + "_23dc" + " temp.jpg " + str(i) + "_out.jpg")
+		print c
+	
+		# sample output: Total time elapsed : 46321 us
+		m = re.match("Total time elapsed : (.*) us", c[1])
+		decoding_time = int(m.group(1))
+		total_decoding_time += decoding_time
+		c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -outputcoef libjpeg.block " + str(i) + "_out.jpg temp.jpg")
+		c = commands.getstatusoutput("diff libjpeg.block py.block")
+		os.system("diff libjpeg.block py.block")
+		if (c[1] != ""):
+			print c[1]
+	if first_seen == False:
+		base_encode = total_encoding_time/100000.0
+		base_decode = total_decoding_time/100000.0
+		base_x = int(f)
+		first_seen = True
+	break
+	x.append(int(f)*int(f))
+	y_encoding.append(base_encode*(int(f)/base_x)*(int(f)/base_x))
+	y_decoding.append(base_decode*(int(f)/base_x)*(int(f)/base_x))
+	encoding.append(total_encoding_time/100000.0)
+	decoding.append(total_decoding_time/100000.0)
+	gain_ratio.append((total_optimized_size-total_encoded_size)*1.0/total_optimized_size)
+	print f
+	print "total opt size:", total_optimized_size, "total encoded size:", total_encoded_size, "(", (total_optimized_size-total_encoded_size)*1.0/total_optimized_size, " saving)"
+	print "total encoding time:", total_encoding_time, "ms (", total_encoding_time/100000.0, "ms per image)"
+	print "total decoding time:", total_decoding_time, "ms (", total_decoding_time/100000.0, "ms per image)"
+
