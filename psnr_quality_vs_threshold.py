e9d54a051a81944800eb29933b0ff74831a5fc7f
vess527 <vess527@gmail.com>
2015-03-11 18:23:18 -0700

    a

--- psnr_quality_vs_threshold.py
+++ psnr_quality_vs_threshold.py
@@ -12,16 +12,16 @@ def get_threshold_jpg(out_, threshold, block_file, base_file, quality):
 	print "(", c[1], 
 	c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -inputcoef tmp_out.block %s %s"%(base_file, out_))
 
-#reso = "600"
-#fs = glob.glob("images/TESTIMAGES/RGB/RGB_R02_0600x0600/*.png")
-reso = "1200"
-fs = glob.glob("images/TESTIMAGES/RGB/RGB_OR_1200x1200/*.png")
-fs = fs[:20]
+reso = "600"
+fs = glob.glob("images/TESTIMAGES/RGB/RGB_R02_0600x0600/*.png")
+#reso = "1200"
+#fs = glob.glob("images/TESTIMAGES/RGB/RGB_OR_1200x1200/*.png")
+fs = fs[:1]
 
 
 qs = range(60, 91)
 qs = [30,40,50,60,70,80,90]
-qs = range(40, 95, 5)
+qs = range(40, 95, 15)
 print qs
 #qs = [30, 50, 70]
 #thre = [0,1.0/8/8/3,1.0/8/8/2,1.0/8/8,3.0/8/8] # 0 is for original (no thresholding)
@@ -39,10 +39,12 @@ folder = ""
 
 x = {}
 y = {}
+y2 = {}
 s_r = {} # size reduction
 for t in thre:
 	x[t] = []
 	y[t] = []
+	y2[t] = []
 	s_r[t] = []
 
 for q in qs:
@@ -54,10 +56,12 @@ for q in qs:
 
 	ind=0
 	psnr = {}
+	ssim = {}
 	size = {}
 	for t in thre:
 		psnr[t] = 0.0
 		size[t] = 0
+		ssim[t] = 0.0
 
 	for f in fs:
 		ind += 1
@@ -66,26 +70,36 @@ for q in qs:
 		c = commands.getstatusoutput("convert -quality " + str(q)  + " "  + f + " " + folder + "/" + str(ind) +".jpg")
 		c = commands.getstatusoutput("/opt/libjpeg-turbo/bin/jpegtran -outputcoef tmp.block %s %s"%(folder+"/"+str(ind)+".jpg", folder+"/"+str(ind)+"_std.jpg"))
 		c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s.jpg tmp_diff.png"%(folder, str(ind)))
-		print "[",c[1], "]",
+		print "[psnr ",c[1], "]",
 		psnr[0] += float(c[1])
+		c = commands.getstatusoutput("pyssim "  + f + " %s/%s.jpg"%(folder, str(ind)))
+		print "[ssim ", c[1],"]"
+		ssim[0] += float(c[1])
 		size[0] += os.path.getsize("%s/%s_std.jpg"%(folder, str(ind)))
 		for t in thre:
 			if t:
+				print t
 				get_threshold_jpg(folder+"/"+str(ind)+"_%d.jpg"%(t), t, "tmp.block", folder+"/"+str(ind)+"_std.jpg", q)
 				c = commands.getstatusoutput("compare -metric PSNR " + f + " %s/%s_%d.jpg tmp_diff.png"%(folder, str(ind), t))
-				print c[1], ")",
+				print "psnr ", c[1], 
 				psnr[t] += float(c[1])
+				c = commands.getstatusoutput("pyssim "  + f + " %s/%s_%d.jpg"%(folder, str(ind), t))
+				print "ssim ", c[1],")",
+				ssim[t] += float(c[1])
 				size[t] += os.path.getsize("%s/%s_%d.jpg"%(folder, str(ind), t))
 		print ""
 	
 	for t in thre:
 		psnr[t] /= len(fs)
 		size[t] /= len(fs)
+		ssim[t] /= len(fs)
 		s_r[t].append(100*(1-size[t]*1.0/size[0]))
 		x[t].append(size[t])
 		y[t].append(psnr[t])
+		y2[t].append(ssim[t])
 print x
 print y
+print y2
 print s_r
 
 fig = plt.figure()
@@ -111,5 +125,31 @@ ax2.set_xlabel("Q")
 ax2.set_ylabel("Bits Saving (%)")
 ax2.set_xlim([min(qs)-3, max(qs)+3])
 tight_layout()
-savefig("psnr_quality_vs_threshold_%s.png"%(reso))
-savefig("psnr_quality_vs_threshold_%s.eps"%(reso))
+savefig("Q_psnr_quality_vs_threshold_%s.png"%(reso))
+savefig("Q_psnr_quality_vs_threshold_%s.eps"%(reso))
+
+fig = plt.figure()
+gs = gridspec.GridSpec(2,1,height_ratios=[2,1])
+ax = plt.subplot(gs[0])
+#ax2 = fig.add_subplot(212)
+ax2 = plt.subplot(gs[1])
+leg = []
+for t in thre:
+	ax.plot(x[t], y2[t], '-x')
+	ax2.plot(qs, s_r[t], '-o')
+	if t:
+		leg.append("th=" + "%.5f"%(t))
+	else:
+		leg.append("original")
+ax.set_xlabel("file size (B)")
+ax.grid()
+ax.set_ylabel("SSIM")
+ax.legend(leg, 4)
+#ax2.legend(leg, 4)
+ax2.grid()
+ax2.set_xlabel("Q")
+ax2.set_ylabel("Bits Saving (%)")
+ax2.set_xlim([min(qs)-3, max(qs)+3])
+tight_layout()
+savefig("Q_ssim_quality_vs_threshold_%s.png"%(reso))
+savefig("Q_ssim_quality_vs_threshold_%s.eps"%(reso))
