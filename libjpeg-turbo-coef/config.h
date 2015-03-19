bbf79093dac416dfdd0b935db4c6336e0a47f730
xingxux <vess527@gmail.com>
2015-03-19 14:29:56 -0700

    a

--- libjpeg-turbo-coef/config.h
+++ libjpeg-turbo-coef/config.h
@@ -5,7 +5,7 @@
 /* #undef BITS_IN_JSAMPLE */
 
 /* libjpeg-turbo build number */
-#define BUILD "20150311"
+#define BUILD "20150318"
 
 /* Support arithmetic encoding */
 #define C_ARITH_CODING_SUPPORTED 1
