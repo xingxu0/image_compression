be4a0f7aa09e5a38c1b6e8c8cd8a389e5c6ef0c6
vess527 <vess527@gmail.com>
2015-03-21 18:23:59 -0700

    a

--- exp_23dc.py
+++ exp_23dc.py
@@ -49,7 +49,7 @@ def get_candidates_size(img_folder):
 
 print "usage: python exp.py; python exp.py 0 if we don't want to calculate our size"
 
-root = "exp_filesize_final_" + str(int(time.time()))
+root = "exp_filesize_pjg_final_" + str(int(time.time()))
 os.system("mkdir %s"%(root))
 f_out = open(root+"/exp.out", "w", 0)
 
